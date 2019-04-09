f2c1051a381ac7be4551d9c37d2847ec39af491c
D7703_19 <d7703_19@d7703-19ui-imac.local>
2019-04-09 15:51:54 +0900

    소수체크

--- PrimeNumber/ViewController.swift
+++ PrimeNumber/ViewController.swift
@@ -7,14 +7,51 @@
 //
 
 import UIKit
+var isPrime = true
+var i = 2
+
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var txta: UITextField!
+   
+    @IBOutlet weak var lbla: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+ 
+    
+    @IBAction func btngo(_ sender: Any) {
+        let number = Int(txta.text!)
+        if number == 1 {
+            isPrime = false
+        }
+        if number == 2{
+            isPrime = true
+        }
+        
+        var i = 2
+        
+        while i < number! {
+            if number! % i == 0 {
+                isPrime = false
+            }
+            i += 1
+        }
+       
+        lbla.text = String(isPrime)
+        isPrime = true
+    
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    // return 키를 누르면 키패드가 내려감
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        txta.resignFirstResponder()
+        return true
+    }
+    
 }
+
 
